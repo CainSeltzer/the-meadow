feb19beff8dc2bd0db6d424c94c6658dcfcf6225
Lucas Kearns <lucasrkearns@gmail.com>
2024-09-19 15:15:41 -0400

    various edits

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,7 +6,6 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
@@ -17,13 +16,15 @@
             </li>
           </ul>
         </div>
-
       </div>
     </nav>
   </div>
 
+  <!--
   <h1>Bleat!</h1>
   <img alt="adorable deer eaing yummy cheezits" width="200" src="./assets/deer-eating-cheezits.gif">
+  -->
+
   <main>
     <RouterView />
   </main>
--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,8 +1,25 @@
 <template>
-    <h2>Mwee?</h2>
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="src/assets/logo.png" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Mwee!</h5>
+                    <p class="card-body">They make lil mwee sounds because they're so small</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </template>
 
-<script set>
+<script>
 export default {
 }
 </script>
+
+<style>
+
+</style>
